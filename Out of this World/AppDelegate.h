403168c1d775769d455b2456e9439f1bee62576c
Admin <admin@Macintosh.local>
2014-10-04 15:44:39 +0700

    Initial Commit

---
+++ Out of this World/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Out of this World
+//
+//  Created by Admin on 10/4/2557 BE.
+//  Copyright (c) 2557 Bonaliza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Out of this World/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Out of this World
+//
+//  Created by Admin on 10/4/2557 BE.
+//  Copyright (c) 2557 Bonaliza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
